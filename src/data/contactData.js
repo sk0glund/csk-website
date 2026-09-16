692ceab06b9b8815a8e338d5f1579babfbe799c7
Adam Skoglund <adam.tiger.skoglund@gmail.com>
2026-09-16 23:19:52 +0100

    temp site

---
+++ src/data/contactData.js
@@ -0,0 +1,11 @@
+export const contactData = {
+    company: "Economic Tool AB",
+    orgnr: "556198-5358",
+    email: "info@economictool.se",
+    phoneLink: "+46706477297",
+    phone: "+46 706 47 72 97",
+    address: "Knätte Långsered 103",
+    zip: "523 91 Ulricehamn",
+    linkedin: "linkedin.com/in/askoglund",
+    linkedinUrl: "https://www.linkedin.com/in/askoglund/",
+};
